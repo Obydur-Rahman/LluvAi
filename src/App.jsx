496d8e19829f59ab387baf3133626d4baa364376
Obydur-Rahman <rahman15-6410@s.diu.edu.bd>
2025-12-04 12:46:59 +0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,13 @@ import Login from "./components/Login";
 import { AppContext } from "./context/AppContext";
 
 const App = () => {
-
   const { showLogin } = useContext(AppContext);
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
       <Navbar />
       {showLogin && <Login />}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/LluvAi" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/buy" element={<BuyCredit />} />
       </Routes>
